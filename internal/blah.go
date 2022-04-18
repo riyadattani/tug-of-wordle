3e55ea1c77c75ac7ecde6175267c51bb258b555d
Riya Dattani <riyaddattani@gmail.com>
2022-04-18 19:26:41 +0100

    first pass

---
+++ internal/blah.go
@@ -0,0 +1,5 @@
+package internal
+
+func GetScore(wordleOutput string) (int, error) {
+	return 3, nil
+}
---
+++ internal/blah_test.go
@@ -0,0 +1,27 @@
+package internal_test
+
+import (
+	"testing"
+
+	"tug-of-wordle/internal"
+)
+
+func TestName(t *testing.T) {
+	t.Run("retrieve wordle score and game number", func(t *testing.T) {
+		wordleOutput := `Wordle 301 3/6
+
+⬜⬜⬜🟨🟨
+🟩🟩🟩⬜⬜
+🟩🟩🟩🟩🟩`
+
+		gotScore, err := internal.GetScore(wordleOutput)
+		if err != nil {
+			t.Errorf("error getting score: %v", err)
+		}
+
+		expectedScore := 3
+		if gotScore != expectedScore {
+			t.Errorf("Expected score to be %d, got %d", expectedScore, gotScore)
+		}
+	})
+}
